8c0db2d33cd825cf2059e7b911f83c31c5eed556
Juan Pablo <juan34063@gmail.com>
2023-12-23 15:53:34 -0300

    Update main.tf with AWS resources

--- test/main.tf
+++ test/main.tf
@@ -1,15 +1,32 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.31.0"
-        }
-    } 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.31.0"
+    }
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_security_group" "default" {
+  name = "default"
+}
+
+data "aws_caller_identity" "current" {}
 
 data "aws_iam_policy_document" "assume_role" {
   statement {
@@ -20,7 +37,10 @@ data "aws_iam_policy_document" "assume_role" {
       identifiers = ["lambda.amazonaws.com"]
     }
 
-    actions = ["sts:AssumeRole"]
+    actions = [
+      "sts:AssumeRole",
+      "sqs:SendMessage"
+    ]
   }
 }
 
@@ -35,6 +55,31 @@ data "archive_file" "lambda" {
   output_path = "lambda_function_payload.zip"
 }
 
+resource "aws_sqs_queue" "test_lambda_dl_queue" {
+  name = "test_lambda_dl_queue"
+}
+
+resource "aws_signer_signing_profile" "default" {
+  name        = "test"
+  platform_id = "AWSLambda-SHA384-ECDSA"
+  signature_validity_period {
+    value = 180
+    type  = "DAYS"
+  }
+}
+
+resource "aws_lambda_code_signing_config" "default" {
+  description = "test"
+
+  allowed_publishers {
+    signing_profile_version_arns = [aws_signer_signing_profile.default.version_arn]
+  }
+
+  policies {
+    untrusted_artifact_on_deployment = "Warn"
+  }
+}
+
 resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
@@ -47,9 +92,20 @@ resource "aws_lambda_function" "test_lambda" {
 
   runtime = "nodejs18.x"
 
-  environment {
-    variables = {
-      foo = "foo"
-    }
+  reserved_concurrent_executions = 1
+
+  dead_letter_config {
+    target_arn = aws_sqs_queue.test_lambda_dl_queue.arn
   }
+
+  tracing_config {
+    mode = "Active"
+  }
+
+  vpc_config {
+    subnet_ids         = data.aws_subnets.default.ids
+    security_group_ids = [data.aws_security_group.default.id]
+  }
+
+  code_signing_config_arn = aws_lambda_code_signing_config.default.arn
 }
